9d0dc0344dcb8451413296c2f0e9c99388507684
kansasdev <mobilne@live.com>
2025-01-17 20:12:43 +0000

    Deploying to gh-pages from @ kansasdev/Site@ceffc6badf33e5d522f3e9b176afb7db4bd944b1 🚀

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+﻿function showEmail(user, domain, elementId) {
+    var email = user + "@" + domain;
+    document.getElementById(elementId).innerText = email;
+}
+function dummyFunction() {
+    alert("test")
+}
